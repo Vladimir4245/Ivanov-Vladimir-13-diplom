6963c5324ae330bc1855375afc015023cbd004d0
Vladimir4245 <vovie@yandex.ru>
2024-02-11 08:31:40 +0000

    Задание на SQL и автотест

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://948e331b-6c9d-414a-8b8f-862fa1c956f3.serverhub.praktikum-services.ru"
+CREATE_COURIER = "/api/v1/courier"
+CREATE_ORDER = "/api/v1/orders"
+ORDER_INFORMATION = "/api/v1/orders/track?t="
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Иванова",
+    "lastName": "Владимир",
+    "address": "Central passage of Khoroshevsky Serebryany Bor, 2 apt.",
+    "metroStation": 5,
+    "phone": "+7 996 332 82 65",
+    "rentTime": 5,
+    "deliveryDate": "2024-02-11",
+    "comment": "Жора привет!",
+    "color": [
+        "BLACK"
+    ]
+}
